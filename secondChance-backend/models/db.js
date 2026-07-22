da340d81dac22eccd795f7384ca998d386031d3b
RCHash <94483717+RCHash@users.noreply.github.com>
2026-07-22 08:30:47 -0400

    Update db.js

--- secondChance-backend/models/db.js
+++ secondChance-backend/models/db.js
@@ -5,7 +5,7 @@ const MongoClient = require('mongodb').MongoClient;
 // MongoDB connection URL with authentication options
 let url = `${process.env.MONGO_URL}`;
 
-let dbInstance = null;
+let dbInstance = connectToDatabase();
 const dbName = `${process.env.MONGO_DB}`;
 
 async function connectToDatabase() {
@@ -16,13 +16,20 @@ async function connectToDatabase() {
     const client = new MongoClient(url);      
 
     // Task 1: Connect to MongoDB
-    // {{insert code}}
+    try {
+        // connect to the mongodb client
+        await client.connect();
+        console.log("Successfully connected to the server");
+    } catch(err) {
+        // log the error
+        console.error(err);
+    }
 
     // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
+    dbInstance=client.db(dbName)
 
     // Task 3: Return database instance
-    // {{insert code}}
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
