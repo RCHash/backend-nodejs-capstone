7231f3137650963aebc3ab6451618822bb730a7e
RCHash <94483717+RCHash@users.noreply.github.com>
2026-07-22 10:03:12 -0400

    Update secondChanceItemsRoutes.js

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -27,12 +27,12 @@ router.get('/', async (req, res, next) => {
     logger.info('/ called');
     try {
         //Step 2: task 1 - insert code here
+        const db = await connectToDatabase();
         //Step 2: task 2 - insert code here
-        //Step 2: task 3 - insert code here
-        //Step 2: task 4 - insert code here
-
         const collection = db.collection("secondChanceItems");
+        //Step 2: task 3 - insert code here
         const secondChanceItems = await collection.find({}).toArray();
+        //Step 2: task 4 - insert code here
         res.json(secondChanceItems);
     } catch (e) {
         logger.console.error('oops something went wrong', e)
@@ -41,7 +41,9 @@ router.get('/', async (req, res, next) => {
 });
 
 // Add a new item
-router.post('/', {Step 3: Task 6 insert code here}, async(req, res,next) => {
+router.post('/', {
+    // Step 3: Task 6 insert code here
+    }, async(req, res,next) => {
     try {
 
         //Step 3: task 1 - insert code here
