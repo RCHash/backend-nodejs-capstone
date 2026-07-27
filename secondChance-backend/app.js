9256219cc1b55a748d9989cf68e9e2e369e13623
RCHash <94483717+RCHash@users.noreply.github.com>
2026-07-27 05:41:16 -0400

    Update app.js

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -8,12 +8,12 @@ const connectToDatabase = require('./models/db');
 // const { loadData } = require('./util/import-mongo/index');
 
 const app = express();
-app.use("*", cors());
+app.use('*', cors());
 const port = 3060;
 
 // Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
-    pinoLogger.info('Connected to DB');
+  pinoLogger.info('Connected to DB');
 })
     .catch((e) => console.error('Failed to connect to DB', e));
 
